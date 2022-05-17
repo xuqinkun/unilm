d3f8119a890791571a66f9076dc2cf55855bf555
xuqinkun <xu_qinkun@163.com>
2022-05-17 12:13:00 +0800

    Add downstream task:Image Text Matching

--- layoutlm/deprecated/layoutlm/modeling/layoutlm.py
+++ layoutlm/deprecated/layoutlm/modeling/layoutlm.py
@@ -296,3 +296,61 @@ class LayoutlmForSequenceClassification(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
+class LayoutlmForImageTextMatching(BertPreTrainedModel):
+    config_class = LayoutlmConfig
+    pretrained_model_archive_map = LAYOUTLM_PRETRAINED_MODEL_ARCHIVE_MAP
+    base_model_prefix = "bert"
+
+    def __init__(self, config):
+        super(LayoutlmForImageTextMatching, self).__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = LayoutlmModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, 2)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids,
+        bbox,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+    ):
+
+        outputs = self.bert(
+            input_ids=input_ids,
+            bbox=bbox,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        outputs = (logits,) + outputs[
+            2:
+        ]  # add hidden states and attention if they are here
+
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), logits, (hidden_states), (attentions)
