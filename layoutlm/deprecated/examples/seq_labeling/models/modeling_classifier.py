47169826557a3d95d86edff30d95a33f613de778
xuqinkun <xu_qinkun@163.com>
2022-05-20 20:47:25 +0800

    Eval metrics

--- layoutlm/deprecated/examples/seq_labeling/models/modeling_classifier.py
+++ layoutlm/deprecated/examples/seq_labeling/models/modeling_classifier.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
+import os
+import torch
+import logging
+import numpy as np
 from pathlib import Path
 
-import torch
 from datasets import load_dataset
-from torch.optim import Adam
-from torch.utils.data import DataLoader, SequentialSampler
+from layoutlmft.data.data_args import XFUNDataTrainingArguments
+from layoutlmft.models.model_args import ModelArguments
+from torch.utils.data import SequentialSampler
 from transformers import AutoTokenizer, HfArgumentParser, TrainingArguments
 from transformers.models.layoutlmv2.configuration_layoutlmv2 import LayoutLMv2Config
 from transformers.trainer import Trainer
-from layoutlm.deprecated.examples.seq_labeling.models.modeling_ITA import ResnetForImageTextMatching
+from transformers.trainer_utils import get_last_checkpoint
+
 import layoutlm.deprecated.examples.seq_labeling.data.sroie as sroie
 from layoutlm.deprecated.examples.seq_labeling.data.data_collator import DataCollatorForClassifier
-from layoutlmft.data.data_args import XFUNDataTrainingArguments
-from layoutlmft.models.model_args import ModelArguments
+from layoutlm.deprecated.examples.seq_labeling.models.modeling_ITA import ResnetForImageTextMatching
+from seqeval.metrics import f1_score, recall_score, precision_score
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logger = logging.getLogger(__file__)
 
 if __name__ == '__main__':
     parser = HfArgumentParser((ModelArguments, XFUNDataTrainingArguments, TrainingArguments))
@@ -34,32 +44,66 @@ if __name__ == '__main__':
         tokenizer=tokenizer,
         version=version,
     )
-    labels = {0: "covered", 1: "uncovered"}
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+    id2label = {0: "covered", 1: "uncovered"}
     train_dataset = dataset['train']
     test_dataset = dataset['test']
-    batch_size = 8
-    label_to_id = {v: k for k, v in labels.items()}
+    label_to_id = {v: k for k, v in id2label.items()}
 
     data_collator = DataCollatorForClassifier(
         tokenizer,
         # pad_to_multiple_of=batch_size,
         padding="max_length",
-        max_length=20,
+        max_length=data_args.max_seq_length,
         label_to_id=label_to_id,
         # device=device,
     )
     sampler = SequentialSampler(train_dataset)
     features = train_dataset.features
-    # train_loader = DataLoader(train_dataset, batch_size=batch_size,
-    #                           sampler=sampler, collate_fn=data_collator)
-    optimizer = Adam(model.parameters(), lr=0.00001)
 
-    model.cuda()
+    def compute_metrics(p):
+        logits, labels = p
+        predictions = np.argmax(logits, axis=2).tolist()
+        # Remove ignored index (special tokens)
+        true_predictions = [
+            [id2label[p] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        y_pred = [p for preds in true_predictions for p in preds]
+        true_labels = [
+            [id2label[l] for (p, l) in zip(prediction, label) if l != -100]
+            for prediction, label in zip(predictions, labels)
+        ]
+        y_true = [l for labels in true_labels for l in labels]
+        num_correct = sum([1 if p == l else 0 for p, l in zip(y_pred, y_true)])
+        return {"accuracy": num_correct/len(y_true)}
+
     trainer = Trainer(model,
                       args=training_args,
                       data_collator=data_collator,
                       train_dataset=train_dataset,
-                      eval_dataset=dataset['test'],
+                      eval_dataset=test_dataset,
                       tokenizer=tokenizer,
+                      compute_metrics=compute_metrics,
                       )
-    trainer.train()
+    if training_args.do_train:
+        trainer.train(resume_from_checkpoint=last_checkpoint)
+    if training_args.do_eval:
+        metrics = trainer.evaluate()
+        eval_dataloader = trainer.get_eval_dataloader(eval_dataset=test_dataset)
+        for k, v in metrics.items():
+            print(f"{k}:{v}")
+    if training_args.do_predict:
+        trainer.predict(test_dataset=test_dataset)
