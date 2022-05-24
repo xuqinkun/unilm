7cb8743a9383f5757fa2d4a6368041fa696559e4
xuqinkun <xu_qinkun@163.com>
2022-05-24 19:32:57 +0800

    ChineseSimilarWords.txt

---
+++ txt/similar_words.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+w = Path("./ChineseSimilarWords.txt")
+lines = w.read_text().split("\n")
+group = {}
+for line in lines:
+    if line.strip() != '':
+        words = set(line.split(","))
+        if '' in words:
+            words.remove('')
+        for word in words:
+            pool = words.copy()
+            pool.remove(word)
+            if word not in group:
+                group[word] = pool
+            else:
+                group[word].update(pool)
+print(len(group.values()))
