9ea27c44f067003478e9259cf6a35c957990d4ec
xuqinkun <xu_qinkun@163.com>
2022-05-24 16:35:44 +0800

    Replace:10% Drop: 10% Insert: 10% Keep: 70%

--- LM-Critic/run_lm_critic.py
+++ LM-Critic/run_lm_critic.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from pathlib import Path
-
 import torch
+import numpy as np
+from pathlib import Path
 from datasets import load_dataset
 from tqdm import tqdm
 from transformers import AutoTokenizer
@@ -17,22 +17,26 @@ if __name__ == '__main__':
         path=Path(xdoc_perturbation.__file__).as_posix(),
         data_dir="/home/std2020/xuqinkun/data/doc_image/contract",
         tokenizer=tokenizer,
-        name='xcontract.en',
+        name='xcontract.zh',
         additional_langs=None,
         keep_in_memory=True,
         doc_type='contract',
         cache_dir=None,
         pred_only=False,
         is_tar_file=True,
-        version='0.0.2',
+        version='0.0.3',
         output_dir='/home/std2020/xuqinkun/model/contract_ITA',
     )
     eval_dataset = dataset['validation']
-    nb_gt = 0
     error_examples = []
     good_examples = []
+    eq_examples = []
     device = 'cuda:0'
-    for feature in tqdm(eval_dataset):
+    max_eval_examples = 500
+    eval_indices = np.arange(len(eval_dataset))
+    np.random.shuffle(eval_indices)
+    eval_samples = eval_dataset.select(eval_indices[:max_eval_examples])
+    for feature in tqdm(eval_samples):
 
         image = torch.tensor(feature['image'], dtype=torch.float32, device=device).unsqueeze(0)
         good_inputs = feature['good_inputs']
@@ -51,17 +55,17 @@ if __name__ == '__main__':
         good_score = good_ret[1]
         bad_score = bad_ret[1]
         if good_score > bad_score:
-            nb_gt += 1
             good_examples.append((good_example, bad_example))
-        else:
+        elif good_score < bad_score:
             error_examples.append((good_example, bad_example))
+        else:
+            eq_examples.append((good_example, bad_example))
 
-    print(f"p(x_good)>p(x_bad): {nb_gt / len(eval_dataset)}\n")
-
-    print("\nGood \tBad\n")
+    print(f"p(x_good)>p(x_bad): {100*len(good_examples) / len(eval_samples):.2f}%\n")
+    print(f"p(x_good)==p(x_bad): {100*len(eq_examples) / len(eval_samples):.2f}%\n")
+    print("\nP(Good) > P(Bad)\n")
     for x1, x2 in good_examples[:10]:
-        print(f"{x1}\t{x2}\n")
-    print("\nGood \tBad\n")
-
+        print(f"p({x1}) > p({x2})")
+    print("\nP(Good) == P(Bad)\n")
     for x1, x2 in error_examples[:10]:
-        print(f"{x1}\t{x2}\n")
+        print(f"p({x1})==p({x2})")
