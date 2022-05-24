d0e5a0ae0a16fc34396d8adbc2f575dd5b9ce826
xuqinkun <xu_qinkun@163.com>
2022-05-24 16:58:36 +0800

    convert_dataset_to_sents

--- LM-Critic/run_lm_critic.py
+++ LM-Critic/run_lm_critic.py
@@ -9,6 +9,24 @@ from transformers import AutoTokenizer
 import layoutlm.deprecated.examples.seq_labeling.data.xdoc_perturbation_score as xdoc_perturbation
 from critic.critic import gpt2_critic
 
+
+def convert_dataset_to_sents(eval_dataset):
+    eval_samples = []
+    for feature in eval_dataset:
+        good_inputs = feature['good_inputs']
+        bad_inputs = feature['bad_inputs']
+        if len(good_inputs) == 0 or len(bad_inputs) == 0 or good_inputs == bad_inputs:
+            continue
+        good_example = tokenizer.convert_ids_to_tokens(good_inputs)
+        bad_example = tokenizer.convert_ids_to_tokens(bad_inputs)
+        good_example = "".join(good_example).replace('▁', '')
+        bad_example = "".join(bad_example).replace('▁', '')
+        if good_example == bad_example:
+            continue
+        eval_samples.append((good_example, bad_example))
+    return eval_samples
+
+
 if __name__ == '__main__':
     version = None
     tokenizer = AutoTokenizer.from_pretrained("/home/std2020/xuqinkun/model/xlm-roberta-base", use_fast=True)
@@ -33,33 +51,24 @@ if __name__ == '__main__':
     eq_examples = []
     device = 'cuda:0'
     max_eval_examples = 500
-    eval_indices = np.arange(len(eval_dataset))
+    eval_samples = convert_dataset_to_sents(eval_dataset)
+    eval_indices = np.arange(len(eval_samples))
     np.random.shuffle(eval_indices)
-    eval_samples = eval_dataset.select(eval_indices[:max_eval_examples])
-    for feature in tqdm(eval_samples):
 
-        image = torch.tensor(feature['image'], dtype=torch.float32, device=device).unsqueeze(0)
-        good_inputs = feature['good_inputs']
-        bad_inputs = feature['bad_inputs']
-        if len(good_inputs) == 0 or len(bad_inputs) == 0:
-            continue
-
-        good_example = tokenizer.convert_ids_to_tokens(good_inputs)
-        bad_example = tokenizer.convert_ids_to_tokens(bad_inputs)
-        good_example = "".join(good_example).replace('▁', '')
-        bad_example = "".join(bad_example).replace('▁', '')
-        good_ret = gpt2_critic(good_example, verbose=0)
-        bad_ret = gpt2_critic(bad_example, verbose=0)
+    eval_samples = [eval_samples[i] for i in eval_indices[:max_eval_examples]]
+    for x_good, x_bad in tqdm(eval_samples):
+        good_ret = gpt2_critic(x_good, verbose=0)
+        bad_ret = gpt2_critic(x_bad, verbose=0)
         if good_ret is None or bad_ret is None:
             continue
         good_score = good_ret[1]
         bad_score = bad_ret[1]
         if good_score > bad_score:
-            good_examples.append((good_example, bad_example))
+            good_examples.append((x_good, x_bad))
         elif good_score < bad_score:
-            error_examples.append((good_example, bad_example))
+            error_examples.append((x_good, x_bad))
         else:
-            eq_examples.append((good_example, bad_example))
+            eq_examples.append((x_good, x_bad))
 
     print(f"p(x_good)>p(x_bad): {100*len(good_examples) / len(eval_samples):.2f}%\n")
     print(f"p(x_good)==p(x_bad): {100*len(eq_examples) / len(eval_samples):.2f}%\n")
